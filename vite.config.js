7d6d511ad482921009d5409a77ff1f105a408427
Elina test <elina.ulli93@gmail.com>
2026-02-05 21:52:49 +0500

    Add GitHub Actions deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'git@github.com:elinaulli/class-component.git'
+  base: '/class-component/'
 })
